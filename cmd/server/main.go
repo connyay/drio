connyay/drio

commit 8b4397fa0a22a63d08ec7117b6b7590e2d29e860

fix: Tighten up static routes.
Getting tons of bot scrapes. Im sure returning 200 is making the bot do additional probes.

cmd/server/main.go
@@ -44,7 +44,11 @@ func (cmd *ServeCmd) Run() error {
 	r.Post("/api/transactions", api.TransactionCreateHandler(store, log))
 	r.Get("/api/transactions", api.TransactionListHandler(store, log))
 	r.Get("/api/totals", api.TotalsHandler(store, log))
-	r.NotFound(web.AssetHandler)
+	r.Get("/static", web.AssetHandler)
+	r.Get("/transactions", web.AssetHandler)
+	r.NotFound(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusNotFound)
+	})
 	log.Printf("Listening on %s", cmd.Addr)
 	return http.ListenAndServe(cmd.Addr, r)
 }

commit 93d2da93236f94f1702c05023b9234184fc13369

fix: Include / in static routes.
Oops. 8b4397fa caused the root route to not load.

cmd/server/main.go
@@ -44,9 +44,14 @@ func (cmd *ServeCmd) Run() error {
 	r.Post("/api/transactions", api.TransactionCreateHandler(store, log))
 	r.Get("/api/transactions", api.TransactionListHandler(store, log))
 	r.Get("/api/totals", api.TotalsHandler(store, log))
-	r.Get("/static", web.AssetHandler)
-	r.Get("/transactions", web.AssetHandler)
 	r.NotFound(func(rw http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet &&
+			(r.RequestURI == "/" ||
+				r.RequestURI == "favicon.ico" ||
+				strings.HasPrefix(r.RequestURI, "/static/")) {
+			web.AssetHandler(rw, r)
+			return
+		}
 		rw.WriteHeader(http.StatusNotFound)
 	})
 	log.Printf("Listening on %s", cmd.Addr)